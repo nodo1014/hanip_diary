11002f26542b27cb98fbb8199a97fc9ac712f8d2
kang <kang@imac-2.local>
2024-06-23 19:40:19 +0900

    emotion 겨ㅅ

--- 12_6_New/src/components/Editor.jsx
+++ 12_6_New/src/components/Editor.jsx
@@ -26,7 +26,8 @@ const emotionList = [
 ]
 
 function Editor() {
-
+    // FIXME: 마우스 오버시, 이모션 배경색 변경! EmotionItem.jsx 에 isSelected(true/false) 프롭스 보내서, true 면, 배경색 변경 클래스 추가 
+    const emotionId =5;
   return (
     <div className="Editor">
       <section className="date_section">
@@ -38,7 +39,7 @@ function Editor() {
         <h4>오늘의 기부니. emotion</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((item) =>(
-            <EmotionItem key={item.emotionId} {...item}/> 
+            <EmotionItem key={item.emotionId} {...item} isSelected={item.emotionId === emotionId} /> 
             ))
             }
         </div>
@@ -46,7 +47,7 @@ function Editor() {
 
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea content="content">content_section</textarea>
+        <textarea placeholder="오늘은 어때쓔? 지금까지와 달라?"></textarea>
       </section>
 
       <section className="button_section">
--- 12_6_New/src/components/EmotionItem.jsx
+++ 12_6_New/src/components/EmotionItem.jsx
@@ -1,11 +1,12 @@
 import "./EmotionItem.css"
 import getEmotionImage from "../util/get-emotion-image";
 
-function EmotionItem({emotionId, emotionName}) {
+function EmotionItem({emotionId, emotionName, isSelected}) {
   return (
-    <div className="EmotionItem">
+    <div className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}`: ""}`}>
       <img className="emotion_img" src={getEmotionImage(emotionId)} />
       <div className="emotion_name">{emotionName}</div>
+
     </div>
   )
 }
